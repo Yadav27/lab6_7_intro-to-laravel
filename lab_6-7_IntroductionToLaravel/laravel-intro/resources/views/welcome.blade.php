93e85b05ce79676d8493469f1a16edcb888f5737
Yadav27 <201901105@daiict.ac.in>
2025-03-22 10:23:17 -0400

    main_commit

---
+++ lab_6-7_IntroductionToLaravel/laravel-intro/resources/views/welcome.blade.php
@@ -0,0 +1,11 @@
+< !DOCTYPE html >
+<html lang="en">
+<head>
+<meta charset="UTF-8" >
+<meta name="viewport" content="width-device-width, initial-scale=1.0"><title>welcome to Laravel</title>
+</head>
+< body>
+<h1>Welcome to Laravel!</h1>
+<p>{{ $message }}</p>
+<p>This is your first Laravel view using Blade templating.</p>
+</body> </html>
